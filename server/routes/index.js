62129d8e0ba6ec700adde822f973dffea8255241
Khoi Phan <jasonphan2631@gmail.com>
2025-08-05 13:54:21 -0700

    hello

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,5 +3,10 @@ const apiRoutes = require("./api");
 
 // All API routes will be prefixed with /api
 router.use("/api", apiRoutes);
+// If no API routes are hit, send the React app
+// Optional: Add 404 fallback for unknown routes
+router.use((req, res) => {
+  res.status(404).json({ error: "Wrong route! from routes/index.js" });
+});
 
 module.exports = router;
