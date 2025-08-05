b2e7b378ad5d0219a97b702841999235b1e5573a
Khoi Phan <jasonphan2631@gmail.com>
2025-08-05 13:58:33 -0700

    hello

--- server/apolloServer.js
+++ server/apolloServer.js
@@ -63,7 +63,7 @@ const startApolloServer = async () => {
 
   // Need to use the routes after the GraphQL middleware
   // This will allow us to have both GraphQL and RESTful API routes in the same
-  app.use(routes); // Loads routes/index.js
+  // app.use(routes); // Loads routes/index.js
 
   if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../client/dist")));
