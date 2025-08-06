f4db165bcfd0dead7de233ac4881f61562b8d3a6
Khoi Phan <jasonphan2631@gmail.com>
2025-08-06 11:27:07 -0700

    444

--- client/src/utils/socket-client.js
+++ client/src/utils/socket-client.js
@@ -1,3 +1,4 @@
+/*
 import { io } from "socket.io-client";
 // this file is used to create a Socket.IO client instance
 // and export it for use in other parts of the application
@@ -8,14 +9,15 @@ const socket = io(import.meta.env.VITE_SOCKET_URL || "http://localhost:3001", {
   autoConnect: false, // Manual connect later when user exists
 });
 export default socket;
+*/
 
-// import { io } from "socket.io-client";
+import { io } from "socket.io-client";
 
-// const socket = io("/", {
-//   transports: ["websocket"],
-//   reconnection: true,
-//   autoConnect: false,
-// });
+const socket = io("/", {
+  transports: ["websocket"],
+  reconnection: true,
+  autoConnect: false,
+});
 
-// export default socket;
+export default socket;
 //When you deploy to Render, your frontend and backend live at the same domain:
